a64884861580049435a03592260916209624cd6f
SHakhnoza <yusupova.shakhnoza.2003@mail.ru>
2023-03-02 03:01:40 +0500

    VueHW1

---
+++ app.js
@@ -0,0 +1,23 @@
+const app = {
+    data() {
+        return {
+            words: '',
+            background: '',
+            green: 'green', 
+            red: 'red', 
+            orange: 'orange', 
+        }
+    },
+    methods: {
+        
+    }
+}
+
+
+
+
+
+
+
+
+Vue.createApp(app).mount('#app')
